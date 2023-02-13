5e383ef3636e43379a308bea14c5b238d9443b60
Ianana1111 <ian52759@gmail.com>
2023-02-14 01:42:51 +0800

    Create StdCon.dart

---
+++ lib/pages/StdCon.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+
+class StdCon extends StatelessWidget {
+  const StdCon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const appTitle = "Data Storing via Blockchain";
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(appTitle),
+      ),
+    );
+  }
+}
