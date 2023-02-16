626c1210a4a40125d52f29cefcf5c1a53c171a53
Ianana1111 <ian52759@gmail.com>
2023-02-16 20:24:45 +0800

    Create ShowKey.dart

---
+++ lib/pages/ShowKey.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShowKey extends StatefulWidget {
+  const ShowKey({super.key});
+
+  @override
+  State<ShowKey> createState() => _ShowKeyState();
+}
+
+class _ShowKeyState extends State<ShowKey> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
