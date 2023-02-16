9f72e0d143720a10696d79e58a9d8e0f7d40bc26
Ianana1111 <ian52759@gmail.com>
2023-02-16 20:24:27 +0800

    Update home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,46 +10,67 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       appBar: AppBar(
-        backgroundColor: Colors.grey[600],
+        backgroundColor: Color.fromARGB(255, 105, 94, 179),
         title: const Text('Data Storing via Blockchain'),
         centerTitle: true,
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Column(
-            children: const <Widget>[
-              SizedBox(height: 200.0),
-              Text(
-                '合約上鏈',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18.0,
-                  fontFamily: 'Fradoka',
-                ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                children: const <Widget>[
+                  SizedBox(height: 200.0),
+                  Text(
+                    '合約上鏈',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18.0,
+                      fontFamily: 'Fradoka',
+                    ),
+                  ),
+                  SizedBox(height: 4.0),
+                  HovBut1(),
+                ],
               ),
-              SizedBox(height: 4.0),
-              HovBut1(),
+              const SizedBox(width: 50.0),
+              Column(
+                children: const <Widget>[
+                  SizedBox(height: 200.0),
+                  Text(
+                    '定型化契約',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18.0,
+                    ),
+                    ),
+                  SizedBox(height: 4.0),
+                  HovBut2(),
+                ],
+                ),
             ],
           ),
-          const SizedBox(width: 50.0),
-          Column(
-            children: const <Widget>[
-              SizedBox(height: 200.0),
-              Text(
-                '定型化契約',
+          const SizedBox(height: 10.0),
+          Container(
+            child: ElevatedButton(
+              child: Text(
+                '檢查合約',
                 style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18.0,
-                ),
-                ),
-              SizedBox(height: 4.0),
-              HovBut2(),
-            ],
-            )
-          
+                  color: Colors.white,
+                )
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor:Color.fromARGB(255, 62, 161, 243),
+                padding: EdgeInsets.symmetric(horizontal: 100, vertical: 20),
+              ),
+              onPressed: (){
+                Navigator.pushNamed(context, '/check');
+              }
+            ),
+          ),
         ],
-      )
+      ),  
     );
   }
 }
