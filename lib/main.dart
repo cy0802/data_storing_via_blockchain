e6f22254b8e07b53f86a90a1497d761e5f788070
Ianana1111 <ian52759@gmail.com>
2023-02-16 20:24:21 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:data_storing_via_blockchain/pages/NormCon.dart';
 import 'package:data_storing_via_blockchain/pages/StdCon.dart';
+import 'package:data_storing_via_blockchain/pages/check.dart';
 import 'package:data_storing_via_blockchain/pages/home.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,7 @@ void main() {
       '/home': (context) => const Home(),
       '/NormCon': (context) => const NormCon(),
       '/StdCon': (context) => const StdCon(),
+      '/check': (context) => const CheckContract(),
     }
   ));
 }
