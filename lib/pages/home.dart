f6ba467b25eef587501d4c1e2a06c0905f1d0060
Ianana1111 <ian52759@gmail.com>
2023-02-25 11:15:08 +0800

    Update home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,8 @@
-import 'package:data_storing_via_blockchain/provider/GoogleAct.dart';
+import 'package:data_storing_via_blockchain/Classes/ShowDialog.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+//import 'package:provider/provider.dart';
+
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -13,18 +14,21 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 105, 94, 179),
-          title: const Text('Data Storing via Blockchain'),
-          actions: [
-            TextButton(
-              child: const Text('Logout'),
-              onPressed: () {
-                final provider =
-                    Provider.of<GoogleSignInProvider>(context, listen: false);
-                provider.googleLogout();
-              },
-            )
-          ]),
+        backgroundColor: const Color.fromARGB(255, 105, 94, 179),
+        title: const Text('Data Storing via Blockchain'),
+        actions: [
+          IconButton(
+          onPressed: ()=> show_dialog(context),
+           icon: Icon(Icons.account_circle_outlined),
+          ),
+          
+        ]
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.grey[800],
+        child: const Icon(Icons.add),
+      ),
       body: Column(
         children: [
           const Spacer(flex: 4),
@@ -107,18 +111,20 @@ class Home extends StatelessWidget {
           const Spacer(flex: 1),
           Container(
             child: ElevatedButton(
-                child: const Text('我的合約',
-                    style: TextStyle(
-                      color: Colors.white,
-                    )),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 62, 161, 243),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 100, vertical: 20),
-                ),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/RecordedCon');
-                }),
+              child: const Text(
+                '我的合約',
+                style: TextStyle(
+                  color: Colors.white,
+                )
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor:const Color.fromARGB(255, 62, 161, 243),
+                padding: const EdgeInsets.symmetric(horizontal: 100, vertical: 20),
+              ),
+              onPressed: (){
+                Navigator.pushNamed(context, '/RecordedCon');
+              }
+            ), 
           ),
           const Spacer(flex: 4),
         ],
