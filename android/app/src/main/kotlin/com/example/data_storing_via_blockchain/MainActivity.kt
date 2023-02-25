7282089aa356131a79b6c605c7602893c8b13d65
Ianana1111 <ian52759@gmail.com>
2023-02-25 11:14:20 +0800

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/data_storing_via_blockchain/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/data_storing_via_blockchain/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.example.data_storing_via_blockchain
 
-import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+import io.flutter.embedding.android.FlutterActivity
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.annotation.NonNull
+
+class MainActivity : FlutterActivity() {
+
+
 }
