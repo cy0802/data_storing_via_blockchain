da4f40488f0a6849a595cb43f8334fdfbcb42923
Ianana1111 <ian52759@gmail.com>
2023-02-25 11:14:34 +0800

    Create ShowDialog.dart

---
+++ lib/Classes/ShowDialog.dart
@@ -0,0 +1,37 @@
+import 'package:data_storing_via_blockchain/provider/GoogleAct.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void show_dialog(BuildContext context) =>  showDialog(
+  context: context,
+  builder: (BuildContext context) => SimpleDialog(
+    title: const Text('your email'),
+    children: <Widget>[
+      Divider(color: Colors.grey[500]),
+      SimpleDialogOption(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 15),
+        onPressed: () => Navigator.pop(context, 'Cancel'),
+        child: const Text('Instruction', style: TextStyle(fontSize: 16)),
+      ),
+
+      SimpleDialogOption(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 15),
+        onPressed: () {
+          Navigator.pushNamed(context, '/Setting');
+        },
+        child: const Text('Setting', style: TextStyle(fontSize: 16)),
+      ),
+      
+      SimpleDialogOption(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 15),
+        child: const Text('Logout', style: TextStyle(fontSize: 16)),
+        onPressed: () {
+          Navigator.pop(context, '');
+          final provider =
+            Provider.of<GoogleSignInProvider>(context, listen: false);
+          provider.googleLogout();
+        },
+      ),
+    ],
+  ), 
+);
