6ba312b442b191876c0424c3b8d9be0d9e401d2a
Ianana1111 <ian52759@gmail.com>
2023-03-02 23:04:21 +0800

    Update user.dart

---
+++ lib/Classes/user.dart
@@ -0,0 +1,27 @@
+class User {
+  final String name;
+  final String contractname;
+  final String another_email;
+  final String have_checked;
+  
+  User({
+    required this.name,
+    required this.contractname,
+    required this.another_email,
+    required this.have_checked,
+  });
+
+  Map<String, dynamic> toJson() =>{
+    'name': name,
+    'contractname': contractname,
+    'another_email': another_email,
+    'have_checked': have_checked,
+  };
+  
+  static User fromJson(Map<String, dynamic> json)=>User(
+    name: json['name'],
+    contractname: json['contractname'],
+    another_email: json['another_email'],
+    have_checked: json['have_checked'],
+  );
+}
