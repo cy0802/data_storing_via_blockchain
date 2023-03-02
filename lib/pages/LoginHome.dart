b014b5bb74b9250a9636bf3647ed8a7011b12b08
Ianana1111 <ian52759@gmail.com>
2023-03-02 23:05:00 +0800

    Update LoginHome.dart

--- lib/pages/LoginHome.dart
+++ lib/pages/LoginHome.dart
@@ -1,5 +1,5 @@
 import 'package:data_storing_via_blockchain/pages/home.dart';
-import 'package:data_storing_via_blockchain/widget/SignUp.dart';
+import 'package:data_storing_via_blockchain/pages/widget/SignUp.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +13,7 @@ class HomePage extends StatelessWidget {
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot){
           if(snapshot.connectionState == ConnectionState.waiting){
-            return Center(child: CircularProgressIndicator());
+            return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasData){
             return Home();
           } else if(snapshot.hasError){
@@ -25,4 +25,4 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
-}
+} 
