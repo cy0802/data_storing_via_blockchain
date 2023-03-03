9093bd69fc016b6355508b1af9a5496c5d5d8b8f
Ianana1111 <ian52759@gmail.com>
2023-03-03 09:51:46 +0800

    Create ShowFile.dart

---
+++ lib/pages/kids/NormalContract/ShowFile.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+// ignore: depend_on_referenced_packages
+import 'package:path/path.dart';
+
+class PDFViewPage extends StatefulWidget {
+
+  final File file;
+
+  const PDFViewPage({
+    super.key, 
+    required this.file,
+  });
+
+  @override
+  State<PDFViewPage> createState() => _PDFViewPageState();
+}
+
+class _PDFViewPageState extends State<PDFViewPage> {
+  @override
+  Widget build(BuildContext context) {
+    final name = basename(widget.file.path);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(name),
+      ),
+      body: PDFView(
+        filePath: widget.file.path,
+      )
+    );
+  }
+}
+/*Scaffold(
+      body: SfPdfViewer.file(
+              File('storage/emulated/0/Download/flutter-succinctly.pdf')));*/
