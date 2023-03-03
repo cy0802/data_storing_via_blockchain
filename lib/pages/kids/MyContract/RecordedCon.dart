8cfb350a371f9f31a6072aff378dbb305b5fba57
Ianana1111 <ian52759@gmail.com>
2023-03-03 09:51:42 +0800

    Create RecordedCon.dart

---
+++ lib/pages/kids/MyContract/RecordedCon.dart
@@ -0,0 +1,36 @@
+import 'package:data_storing_via_blockchain/pages/kids/MyContract/MyConSubPage/signed.dart';
+import 'package:data_storing_via_blockchain/pages/kids/MyContract/MyConSubPage/unsign.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class History extends StatefulWidget {
+  const History({super.key});
+
+  @override
+  State<History> createState() => _HistoryState();
+}
+
+class _HistoryState extends State<History> {
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('History contract'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: '待簽署', icon: Icon(Icons.border_color)),
+              Tab(text: '已簽署', icon: Icon(Icons.assignment_turned_in)),
+            ],
+          )
+        ),
+        body: const TabBarView(
+          children: [UnSignCon(), SignedCon()],
+        )
+      ),
+    );
+  }
+} 
