d76c798ba58fa59f51c1488708494df88fd2121a
Ianana1111 <ian52759@gmail.com>
2023-03-15 00:08:30 +0800

    Update unsign.dart

--- lib/pages/kids/MyContract/MyConSubPage/unsign.dart
+++ lib/pages/kids/MyContract/MyConSubPage/unsign.dart
@@ -26,86 +26,132 @@ class UnSignCon extends StatefulWidget {
 
 class _UnSignConState extends State<UnSignCon> {
   late String email;
-  //late bool user2_have_check;
-  //late bool user1_have_check;
+  String userorder = "";
+  bool user2HaveCheck = false;
+  bool user1HaveCheck = false;
+  String user2Time="";
+  bool e = true;
+
+  
+  
   @override
   Widget build(BuildContext context) {
 
     final tmp = Provider.of<GoogleSignInProvider>(context);
     email= tmp.user.email;
 
-    return StreamBuilder<List<User>>(
-      stream: readUsers(email),
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance
+      .collection('user').doc(email).collection('contract')
+      .snapshots(),
       builder: (context, snapshot){
-        
         if (snapshot.hasError){
           return Text('Something went Wrong! ${snapshot.error}');
-
+        
         } else if(snapshot.hasData){
-          final users = snapshot.data!;
-          return ListView(
-            children: users.map(buildUser).toList(),
-          );
+          return ListView.builder(
+              shrinkWrap: true,
+              itemCount: snapshot.data!.docs.length, 
+              itemBuilder: (context, index) {
 
+                var data = snapshot.data!.docs[index].data() as Map<String, dynamic>;
+                userorder = data['order'];
+                user1HaveCheck = data['have_checked'];
+                String anotherEmail1 = data['another_email'];
+                final snap =
+                  FirebaseFirestore.instance.collection("user")
+                  .doc(anotherEmail1)
+                  .collection("contract")
+                  .doc(data['contractname'])
+                  .get().then(
+                  (DocumentSnapshot doc) {
+                    final datas = doc.data() as Map<String, dynamic>;
+                    if(e){
+                      setState(() {
+                      user2Time = datas['time'];
+                      user2HaveCheck = datas['have_checked'];
+                      e = false;
+                    });
+                    }
+                  },
+                );
+
+                return Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Color.fromARGB(255, 209, 208, 208),
+                      ),
+                      title: Text(
+                        data['contractname'],
+                        style: TextStyle(
+                          fontSize: 20,
+                        )
+                      ),
+                      subtitle: Text(data['name']),
+                      trailing: hi(),
+                      onTap: (){
+                        print(user2Time);
+                        if(data['have_checked'] && user2HaveCheck){
+                          if(userorder=="first"){
+                            Show_data1(context, data['contractname'], data['name'], data['time'], user2Time);
+                          }else{
+                            Show_data4(context, data['contractname'], data['name'], data['time']);
+                          }
+                          
+                        } else if (data['have_checked']){
+                          Show_data2(context, data['contractname'], data['name'], data['time']);
+                        } else {
+                          Show_data3(context, data['contractname'], data['name'], data['time']);
+                        }
+      
+                      }
+                    )
+                  );
+              },
+            );
         } else if (snapshot.connectionState == ConnectionState.waiting){
           return const Center(child: CircularProgressIndicator());
         } else {
           return Text('no data');
         }
       }
-      
     );
-    
   }
-  
-  Stream<List<User>> readUsers(String email) => FirebaseFirestore.instance
-      .collection('user').doc(email).collection('contract')
-      .snapshots()
-      .map((snapshot)=>
-              snapshot.docs.map((doc)=> User.fromJson(doc.data())).toList());
-  
-  Widget buildUser(User user) => 
-    Card(
-      child: ListTile(
-        leading: Icon(Icons.upload),
-        title: Text(user.contractname),
-        subtitle: Text(user.name),
-        onTap: ()async{
-          //找資料
-          DocumentSnapshot snap1 = await FirebaseFirestore.instance.collection("user").doc(email).collection("contract").doc(user.contractname).get();
-          String anotherEmail1 = snap1['another_email'];
-          DocumentSnapshot snap2 = await FirebaseFirestore.instance.collection("user").doc(anotherEmail1).collection("contract").doc(user.contractname).get();
-          bool user2_have_check = snap2['have_checked'];
-          bool user1_have_check = user.have_checked;
-          String user2Time = snap2['time'];
-          
-
-          if(user.have_checked && user2_have_check){
-            if(user.order=="first"){
-              Show_data1(context, user.contractname, user.name, user.time, user2Time);
-            }else{
-              Show_data4(context, user.contractname, user.name, user.time);
-            }
-            
-          } else if (user.have_checked){
-            Show_data2(context, user.contractname, user.name, user.time);
-          } else {
-            Show_data3(context, user.contractname, user.name, user.time);
-          }
-            
-        },
-      ),
-      
-    );
-    /*hi() {
-      if(user1_have_check && user2_have_check){
-        return Icon(Icons.upload); 
-      } else if (user1_have_check){
-        return Icon(Icons.flaky_rounded); 
+    hi() {
+      if(userorder=="first"){
+        if(user2HaveCheck){
+          return Text(
+            '等待上鏈',
+            style: TextStyle(
+              color: Color.fromARGB(255, 53, 137, 56),
+            ),
+          );
+        } else {
+          return Text(
+            '等待對方同意',
+            style: TextStyle(
+              color: Colors.blue,
+            ),
+          );
+        }
       } else {
-        return Icon(Icons.drive_file_rename_outline_rounded); 
+        if(user1HaveCheck){
+          return Text(
+            '等待對方上鏈',
+            style: TextStyle(
+              color: Color.fromARGB(255, 53, 137, 56),
+            ),
+          );
+        } else {
+          return Text(
+            '需同意',
+            style: TextStyle(
+              color: Colors.blue,
+            ),
+          );
+        }
       }
-    }*/
+    }
 }
 
 
@@ -117,3 +163,15 @@ void Show_data3(BuildContext context, String contractname, String name, String t
     .push(MaterialPageRoute(builder: (context) => WaitToSign(contractname: contractname, name: name, time: time)));
 void Show_data4(BuildContext context, String contractname, String name, String time) => Navigator.of(context)
     .push(MaterialPageRoute(builder: (context) => ShowInfo(contractname: contractname, name: name, time: time)));
+
+
+/*
+.then(
+                    (DocumentSnapshot doc){
+                      final data2 = doc.data() as Map<String, dynamic>;
+                      setState(() async{
+                        user2Time = data2['time'];
+                        user2HaveCheck = data2['have_checked'];
+                      });
+                    }
+                  );*/
